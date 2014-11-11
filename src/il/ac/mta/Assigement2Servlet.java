6fdf60b91f397676a2d290c17481db30652402a0
morsigler <morsigler@gmail>
2014-11-11 17:59:13 +0200

    assigement2

---
+++ src/il/ac/mta/Assigement2Servlet.java
@@ -0,0 +1,16 @@
+package il.ac.mta;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class Assigement2Servlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		resp.setContentType("text/html");
+		int num1; int num2; int result;
+		num1 = 2; num2= 3; result = 2*3;
+		String resultStr = new String("the result of"+num1+"*"+num2+"="+result);
+		resp.getWriter().println(resultStr);
+	}
+}
