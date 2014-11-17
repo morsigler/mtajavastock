f0a399aff7cdff088dd32ba72672656bfe328551
morsigler <morsigler@gmail>
2014-11-17 16:03:58 +0200

    first part exercise 3

--- src/il/ac/mta/Assigement2Servlet.java
+++ src/il/ac/mta/Assigement2Servlet.java
@@ -1,6 +1,7 @@
 package il.ac.mta;
 
 import java.io.IOException;
+
 import javax.servlet.http.*;
 
 @SuppressWarnings("serial")
@@ -8,9 +9,18 @@ public class Assigement2Servlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
 		resp.setContentType("text/html");
-		int num1; int num2; int result;
-		num1 = 2; num2= 3; result = 2*3;
-		String resultStr = new String("the result of "+num1+" * "+num2+" = "+result);
+		int num1; int num2; int num3; int result;
+		num1 = 4; num2= 3; num3=7; result = (num1+num2)*num3;
+		String resultStr = new String("<h1>the result of "+num1+" + "+num2+" * "+num3+" = "+result+"<h1>");
 		resp.getWriter().println(resultStr);
+	
+		double radius, area; 
+	
+		radius = 0.5;
+		area = Math.PI * Math.pow(radius, 2);
+		String resultArea = new String(" Area of circle with radius "+radius+" is: "+area+" square-cm");
+		resp.getWriter().println(resultArea);
+		
+		
 	}
 }
