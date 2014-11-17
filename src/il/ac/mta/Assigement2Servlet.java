b9ddcaaa0b95ca5a52841f0e958f50e93bc5f19b
morsigler <morsigler@gmail>
2014-11-17 17:04:42 +0200

    part 2 exercise 3

--- src/il/ac/mta/Assigement2Servlet.java
+++ src/il/ac/mta/Assigement2Servlet.java
@@ -9,18 +9,27 @@ public class Assigement2Servlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
 		resp.setContentType("text/html");
+		//exercise 2
 		int num1; int num2; int num3; int result;
 		num1 = 4; num2= 3; num3=7; result = (num1+num2)*num3;
 		String resultStr = new String("<h1>the result of "+num1+" + "+num2+" * "+num3+" = "+result+"<h1>");
 		resp.getWriter().println(resultStr);
 	
+		//exercise 3
+		//part 1
 		double radius, area; 
-	
-		radius = 0.5;
+		radius = 50;
 		area = Math.PI * Math.pow(radius, 2);
-		String resultArea = new String(" Area of circle with radius "+radius+" is: "+area+" square-cm");
+		String resultArea = new String("Area of circle with radius "+radius+" is: "+area+" square-cm");
 		resp.getWriter().println(resultArea);
-		
+	
+		//part2
+		double angle, hypotenuse, angleInRadians, opposite ;
+		angle = 30; hypotenuse = 50;
+		angleInRadians = Math.toRadians(angle);
+		opposite = hypotenuse* Math.sin(angleInRadians);
+		String resultOpposite = new String("Length of opposite where angle B is 30 degrees and  Hypotenuse length is 50 cm is: "+opposite+" cm");
+		resp.getWriter().println(resultOpposite);
 		
 	}
 }
